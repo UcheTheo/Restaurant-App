aba40da4118619fa068eaea7da0c8f176137e1b7
UcheTheo <theo.uche2023@gmail.com>
2024-09-01 10:01:40 +0100

    Add the resgister-user endpoint to the users.service

--- server/apps/users/src/users.service.ts
+++ server/apps/users/src/users.service.ts
@@ -1,15 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'prisma/prisma.service';
+import { RegisterDto } from './dtos/user.dto';
+import * as bcrypt from 'bcrypt';
+import { Response } from 'express';
 
 @Injectable()
-export class UsersService {
+export class UsersService
+{
   constructor(
     private readonly jwtService: JwtService,
-    // private readonly prisma: PrismaService,
+    private readonly prisma: PrismaService,
     private readonly configService: ConfigService,
-  ) {}
+  ) { }
 
   // register user service
-  
+  async register(registerDto: RegisterDto, response: Response)
+  {
+    const { name, email, password, phone_number } = registerDto;
+
+    const isEmailExist = await this.prisma.user.findUnique({
+      where: {
+        email,
+      },
+    });
+    if (isEmailExist)
+    {
+      throw new BadRequestException('User already exist with this email!');
+    }
+
+    const phoneNumbersToCheck = [phone_number];
+
+    const usersWithPhoneNumber = await this.prisma.user.findMany({
+      where: {
+        phone_number: {
+          not: null,
+          in: phoneNumbersToCheck,
+        },
+      },
+    });
+
+    if (usersWithPhoneNumber.length > 0)
+    {
+      throw new BadRequestException(
+        'User already exist with this phone number!',
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = {
+      name,
+      email,
+      password: hashedPassword,
+      phone_number,
+    };
+
+
+    return user;
+  }
 }
